85876d3c29ed57b39e90e43253ad3358c1ade1b3
5pence5 <eamesjcb@gmail.com>
2025-02-27 17:22:02 +0000

    Update scrape.sh

--- scrape.sh
+++ scrape.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-# ./download.sh 'https://www.example.com/'
+
+# Create data directory if it doesn't exist
+mkdir -p data
+
+# Run the Scrapy spider
+cd openai_scraper
+scrapy crawl openai
